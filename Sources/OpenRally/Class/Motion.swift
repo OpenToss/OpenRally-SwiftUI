ec7c54d9c7bc32681e7009ce4ffb24b4c634f642
Mercen <mercen@mercen.net>
2023-07-21 11:40:41 +0900

    UPDATE: Methods

--- Sources/OpenRally/Class/Motion.swift
+++ Sources/OpenRally/Class/Motion.swift
@@ -5,7 +5,28 @@ public class Motion {
     
     let easing: any Easing
     
+    var scale: CGFloat = 0
+    var opacity: CGFloat = 0
+    var translateX: CGFloat = 0
+    var translateY: CGFloat = 0
+    
     public init(easing: any Easing, duration: CGFloat = 1) {
         self.easing = easing
+    }
+    
+    public func scale(from: CGFloat, to: CGFloat) {
+        
+    }
+    
+    public func opacity(from: CGFloat, to: CGFloat) {
+        
+    }
+    
+    public func translateX(from: CGFloat, to: CGFloat) {
+        
+    }
+    
+    public func translateY(from: CGFloat, to: CGFloat) {
+        
     }
 }
---
+++ Sources/OpenRally/Preview/Preview.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+@available(macOS 11, iOS 14, *)
+struct Preview: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+@available(macOS 11, iOS 14, *)
+struct Preview_Previews: PreviewProvider {
+    static var previews: some View {
+        Preview()
+    }
+}
